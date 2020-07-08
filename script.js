2da07e10dac1277f2f1c8b447a08996ebe1aba05
Igor Dovganyuk <thelscon@gmail.com>
2020-07-08 23:21:55 +0300

    final

---
+++ script.js
@@ -0,0 +1,28 @@
+'use script' ;
+
+/*          версия for
+for (let i = 0 ; i <= 10 ; i++) {
+    if (!(i%2)) break ;
+    console.log (i) ;
+}
+*/
+
+/*          версия do while
+let i = 1 ;
+
+do {
+    if (!(i%2)) break ;
+    console.log (i) ;
+    ++i ;
+} while (i <= 10) ;
+*/
+
+
+//          версия for
+let i = 1 ;
+
+while (i <= 10) {
+    if (!(i%2)) break ;
+    console.log (i) ;
+    ++i ;
+}
